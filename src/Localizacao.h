6fc699d595b6238f727054431773d3fa33c74290
zecst19 <zecst19@gmail.com>
2016-04-27 23:02:46 +0100

    oi59

--- src/Localizacao.h
+++ src/Localizacao.h
@@ -1,17 +1,19 @@
 /*
- * Criança.h
+ * Localizacao.h
  *
  *  Created on: 19/04/2016
  *      Author: ASUS
  */
-#ifndef SRC_CRIANÇA_H_
-#define SRC_CRIANÇA_H_
+#ifndef SRC_LOCALIZACAO_H_
+#define SRC_LOCALIZACAO_H_
 
 #include <string>
 
 using namespace std;
 
-
+/**
+ * Class where the Localizacao that is set in the Graph.
+ */
 class Localizacao{
 private:
 	string nome;
@@ -19,15 +21,53 @@ private:
 	unsigned int id;
 	string rua;
 public:
+	/**
+	 * \brief Default Constructor with no arguments
+	 */
 	Localizacao();
+	/**
+	 * 	\brief Default Constructor for Localizacao
+	 *  \param nome Localizacao's nome
+	 *  \param id Localizacao's id
+	 *  \param rua Localizacao's rua
+	 */
 	Localizacao(string nome, unsigned int id, string rua);
+	/**
+	 * \return Localizacao's nome
+	 */
 	string getNome() const;
+	/**
+	 *  \brief Change the Localizacao's nome
+	 *  \param id New nome
+	*/
 	void setNome(string nome);
+	/**
+	*  \return Localizacao's ID
+	*
+	*/
 	unsigned int getId() const;
+	/**
+	*  \brief Change the Localizacao's ID
+	*  \param id New ID
+	*/
 	void setId(unsigned int id);
+	/**
+	*  \return Localizacao's next ID
+	*/
 	static unsigned int getNextId();
+	/**
+	*  \brief Change the Localizacao's next ID
+	*  \param nextId New next ID
+	*/
 	static void setNextId(unsigned int nextId);
+	/**
+	 *  \return Localizacao's rua
+	 */
 	string getRua() const;
+	/**
+	 * \brief Change the Localizacao's rua
+	 * \param rua New rua
+	 */
 	void setRua(string rua);
 
 
@@ -36,4 +76,4 @@ public:
 
 
 
-#endif /* SRC_CRIANÇA_H_ */
+#endif /* SRC_LOCALIZACAO_H_ */
